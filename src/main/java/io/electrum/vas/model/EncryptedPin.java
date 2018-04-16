6cf6cdd073162be7d18eb0a342c265ec315d8cdd
welham <matthewwelham@electrum.co.za>
2018-04-16 12:22:04 +0200

    Add .equals, .hashCode and .toString to EncryptedPin

--- src/main/java/io/electrum/vas/model/EncryptedPin.java
+++ src/main/java/io/electrum/vas/model/EncryptedPin.java
@@ -1,12 +1,14 @@
 package io.electrum.vas.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.electrum.vas.Utils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.xml.bind.annotation.XmlElement;
+import java.util.Objects;
 
 /**
  * A PIN required to authorise a transaction.
@@ -139,5 +141,37 @@ public class EncryptedPin {
 
    public void setDukptKsn(String dukptKsn) {
       this.dukptKsn = dukptKsn;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (o == null || getClass() != o.getClass())
+         return false;
+      final EncryptedPin that = (EncryptedPin) o;
+      return Objects.equals(pinBlock, that.pinBlock) && pinBlockFormat == that.pinBlockFormat && Objects.equals(accountNumber,
+            that.accountNumber) && Objects.equals(keyName, that.keyName) && Objects.equals(
+            keyIndex,
+            that.keyIndex) && Objects.equals(dukptKsn, that.dukptKsn);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(pinBlock, pinBlockFormat, accountNumber, keyName, keyIndex, dukptKsn);
+   }
+
+   @Override
+   public String toString() {
+      final StringBuilder sb = new StringBuilder();
+      sb.append("class EncryptedPin {\n");
+      sb.append("    pinBlock: ").append(Utils.toIndentedString(pinBlock)).append("\n");
+      sb.append("    pinBlockFormat: ").append(Utils.toIndentedString(pinBlockFormat)).append("\n");
+      sb.append("    accountNumber: ").append(Utils.toIndentedString(accountNumber)).append("\n");
+      sb.append("    keyName: ").append(Utils.toIndentedString(keyName)).append("\n");
+      sb.append("    keyIndex: ").append(Utils.toIndentedString(keyIndex)).append("\n");
+      sb.append("    dukptKsn: ").append(Utils.toIndentedString(dukptKsn)).append("\n");
+      sb.append("}");
+      return sb.toString();
    }
 }
