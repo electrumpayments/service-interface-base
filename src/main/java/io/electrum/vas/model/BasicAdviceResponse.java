a2697be85e88e8081d740046a0cf41c308bf1e8d
Kiuran <kiuran@protonmail.com>
2018-05-25 11:29:10 +0200

    fix [release-type snapshot]

--- src/main/java/io/electrum/vas/model/BasicAdviceResponse.java
+++ src/main/java/io/electrum/vas/model/BasicAdviceResponse.java
@@ -1,6 +1,5 @@
 package io.electrum.vas.model;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,52 +16,49 @@ import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Basic advice response information.
+ * @deprecated Rather use the @link [BasicAdvice} object for Advice responses
  **/
 @ApiModel(description = "Basic advice response information.")
-public class BasicAdviceResponse {
-
-   protected String id;
-   protected String requestId;
-   protected DateTime time;
-   protected List<ThirdPartyIdentifier> thirdPartyIdentifiers = new ArrayList<ThirdPartyIdentifier>();
-
+public class BasicAdviceResponse extends BasicAdvice {
    /**
     * The randomly generated UUID identifying this advice, as defined for a variant 4 UUID in [RFC
     * 4122](https://tools.ietf.org/html/rfc4122)
     **/
    public BasicAdviceResponse id(String id) {
-      this.id = id;
-      return this;
+      return (BasicAdviceResponse) super.id(id);
    }
 
    @ApiModelProperty(required = true, value = "The randomly generated UUID identifying this advice, as defined for a variant 4 UUID in [RFC 4122](https://tools.ietf.org/html/rfc4122)")
    @JsonProperty("id")
    @NotNull
+   @Override
    public String getId() {
-      return id;
+      return super.getId();
    }
 
+   @Override
    public void setId(String id) {
-      this.id = id;
+      super.setId(id);
    }
 
    /**
     * The UUID identifying the request that this advice relates to
     **/
    public BasicAdviceResponse requestId(String requestId) {
-      this.requestId = requestId;
-      return this;
+      return (BasicAdviceResponse) super.requestId(requestId);
    }
 
    @ApiModelProperty(required = true, value = "The UUID identifying the request that this advice relates to")
    @JsonProperty("requestId")
    @NotNull
+   @Override
    public String getRequestId() {
-      return requestId;
+      return super.getRequestId();
    }
 
+   @Override
    public void setRequestId(String requestId) {
-      this.requestId = requestId;
+      super.setRequestId(requestId);
    }
 
    /**
@@ -71,20 +67,21 @@ public class BasicAdviceResponse {
     * time-secfrac be included up to millisecond precision
     **/
    public BasicAdviceResponse time(DateTime time) {
-      this.time = time;
-      return this;
+      return (BasicAdviceResponse) super.time(time);
    }
 
    @ApiModelProperty(required = true, value = "The date and time of the message as recorded by the sender. The format shall be as defined for date-time in [RFC 3339 section 5.6](https://tools.ietf.org/html/rfc3339#section-5.6). It is recommended that the optional time-secfrac be included up to millisecond precision")
    @JsonProperty("time")
    @NotNull
    @Valid
+   @Override
    public DateTime getTime() {
-      return time;
+      return super.getTime();
    }
 
+   @Override
    public void setTime(DateTime time) {
-      this.time = time;
+      super.setTime(time);
    }
 
    /**
@@ -94,20 +91,21 @@ public class BasicAdviceResponse {
     * original request.
     **/
    public BasicAdviceResponse transactionIdentifiers(List<ThirdPartyIdentifier> transactionIdentifiers) {
-      this.thirdPartyIdentifiers = transactionIdentifiers;
-      return this;
+      return (BasicAdviceResponse) super.transactionIdentifiers(transactionIdentifiers);
    }
 
    @ApiModelProperty(required = true, value = "The unaltered thirdPartyIdentifiers array as supplied in the related BasicResponse message. Required if thirdPartyIdentifiers field was present in the BasicResponse. If no thirdPartyIdentifiers was received in the BasicResponse or no BasicResponse was received then this should be set to the thirdPartyIdentifiers sent in the original request.")
    @JsonProperty("thirdPartyIdentifiers")
    @NotNull
    @Valid
+   @Override
    public List<ThirdPartyIdentifier> getThirdPartyIdentifiers() {
-      return thirdPartyIdentifiers;
+      return super.getThirdPartyIdentifiers();
    }
 
+   @Override
    public void setThirdPartyIdentifiers(List<ThirdPartyIdentifier> transactionIdentifiers) {
-      this.thirdPartyIdentifiers = transactionIdentifiers;
+      super.setThirdPartyIdentifiers(transactionIdentifiers);
    }
 
    @Override
