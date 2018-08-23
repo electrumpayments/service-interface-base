ff9668ed7384466be05e11aa4cbe3fa49f06241c
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2018-08-23 17:04:24 +0200

    Added fields to customer and trantype

--- src/main/java/io/electrum/vas/model/Customer.java
+++ src/main/java/io/electrum/vas/model/Customer.java
@@ -1,13 +1,17 @@
 package io.electrum.vas.model;
 
+import java.util.Objects;
+
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.Length;
+import org.joda.time.DateTime;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.electrum.vas.Utils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.hibernate.validator.constraints.Length;
-
-import java.util.Objects;
 
 /**
  * A customer who ultimately requests a transaction be performed.
@@ -18,6 +22,8 @@ public class Customer {
    private String firstName = null;
    private String lastName = null;
    private String address = null;
+   private DateTime dateOfBirth = null;
+   private String status = null;
 
    /**
     * The customer's first name(s)
@@ -76,6 +82,43 @@ public class Customer {
       this.address = address;
    }
 
+   /**
+    * The customer's date of birth
+    **/
+   public Customer dateOfBirth(DateTime dateOfBirth) {
+      this.dateOfBirth = dateOfBirth;
+      return this;
+   }
+
+   @ApiModelProperty(value = "The customer's date of birth")
+   @JsonProperty("dateOfBirth")
+   @Valid
+   public DateTime getDateOfBirth() {
+      return dateOfBirth;
+   }
+
+   public void setDateOfBirth(DateTime dateOfBirth) {
+      this.dateOfBirth = dateOfBirth;
+   }
+
+   /**
+    * The status of this customer on the Giftcard system. For example: active, inactive
+    **/
+   public Customer status(String status) {
+      this.status = status;
+      return this;
+   }
+
+   @ApiModelProperty(value = "The status of this customer on the Giftcard system. For example: active, inactive")
+   @JsonProperty("status")
+   public String getStatus() {
+      return status;
+   }
+
+   public void setStatus(String status) {
+      this.status = status;
+   }
+
    @Override
    public boolean equals(Object o) {
       if (this == o) {
@@ -86,12 +129,13 @@ public class Customer {
       }
       Customer customer = (Customer) o;
       return Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName)
-            && Objects.equals(address, customer.address);
+            && Objects.equals(address, customer.address) && Objects.equals(dateOfBirth, customer.dateOfBirth)
+            && Objects.equals(status, customer.status);
    }
 
    @Override
    public int hashCode() {
-      return Objects.hash(firstName, lastName, address);
+      return Objects.hash(firstName, lastName, address, dateOfBirth, status);
    }
 
    @Override
@@ -102,6 +146,8 @@ public class Customer {
       sb.append("    firstName: ").append(Utils.toIndentedString(firstName)).append("\n");
       sb.append("    lastName: ").append(Utils.toIndentedString(lastName)).append("\n");
       sb.append("    address: ").append(Utils.toIndentedString(address)).append("\n");
+      sb.append("    dateOfBirth: ").append(Utils.toIndentedString(dateOfBirth)).append("\n");
+      sb.append("    status: ").append(Utils.toIndentedString(status)).append("\n");
       sb.append("}");
       return sb.toString();
    }
