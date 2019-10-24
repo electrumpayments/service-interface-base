a265b39583a04180035bd65b289af0883678dffe
Keelan-Moore <keelan.moore@electrum.co.za>
2019-10-24 15:20:33 +0200

    initial changes

--- src/main/java/io/electrum/vas/model/BankAccount.java
+++ src/main/java/io/electrum/vas/model/BankAccount.java
@@ -13,6 +13,8 @@ public class BankAccount extends Account {
 
    private String routingCode = null;
 
+   private String region = null;
+
    public BankAccount() {
       setType(AccountType.BANK);
    }
@@ -32,9 +34,25 @@ public class BankAccount extends Account {
       this.routingCode = routingCode;
    }
 
+   public BankAccount region(String region) {
+      this.region = region;
+      return this;
+   }
+
+   @ApiModelProperty(value = "The region of the account for countries and cases where the routing code is not sufficient to "
+         + "identify a bank account.")
+   @JsonProperty("routingCode")
+   public String getRegion() {
+      return region;
+   }
+
+   public void setRegion(String region) {
+      this.region = region;
+   }
+
    @Override
    public int hashCode() {
-      return Objects.hash(routingCode, type, routingCode);
+      return Objects.hash(routingCode, type, routingCode, region);
    }
 
    @Override
@@ -46,7 +64,8 @@ public class BankAccount extends Account {
          return false;
       }
       BankAccount account = (BankAccount) o;
-      return super.equals(o) && Objects.equals(this.routingCode, account.routingCode);
+      return super.equals(o) && Objects.equals(this.routingCode, account.routingCode)
+            && Objects.equals(this.region, account.region);
    }
 
    @Override
@@ -56,6 +75,7 @@ public class BankAccount extends Account {
       sb.append("    accountId: ").append(Utils.toIndentedString(accountId)).append("\n");
       sb.append("    type: ").append(Utils.toIndentedString(type)).append("\n");
       sb.append("    routingCode: ").append(Utils.toIndentedString(routingCode)).append("\n");
+      sb.append("    region: ").append(Utils.toIndentedString(region)).append("\n");
       sb.append("}");
       return sb.toString();
    }
