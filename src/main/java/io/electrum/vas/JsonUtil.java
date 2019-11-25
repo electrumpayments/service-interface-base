e108fd726d32850298bed9fcd459f75485f917cb
Mike <40794887+MikeField119@users.noreply.github.com>
2019-11-25 11:10:25 +0200

    v3.23.0 [release-type final] Added deserialiseJsonObjectFromFile method
    Added deserialiseJsonObjectFromFile & readFileAsString methods in JsonUtil. Updated README & release notes.

--- src/main/java/io/electrum/vas/JsonUtil.java
+++ src/main/java/io/electrum/vas/JsonUtil.java
@@ -1,5 +1,7 @@
 package io.electrum.vas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,9 +17,12 @@ import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
- * Class to make serialising and deserializing json objects more efficient
+ * Class to make serialising and deserializing json objects more efficient For serializing/deserializing from json file,
+ * ensure the file is in the './src/test/resources/messages/' directory
  */
 public class JsonUtil {
+
+   public static String TEST_MSG_FILE_PATH = "./src/test/resources/messages/";
    private final static ObjectMapper baseMapper = new ObjectMapper();
    private final static Map<Class<?>, ObjectReader> readerCache = new HashMap<>();
    private final static Map<Class<?>, ObjectWriter> writerCache = new HashMap<>();
@@ -99,5 +104,59 @@ public class JsonUtil {
 
    public static ObjectMapper getBaseMapper() {
       return baseMapper;
+   }
+
+   /**
+    * Method to deserialise a Json object from a file.
+    *
+    * @param <T>
+    *           The payload for the json object
+    * @param filename
+    *           The name of the file with the JSon object
+    * @param clazz
+    *           name of Class type to be deserialized into
+    * @param preserveWhitespace
+    *           whether to include spaces/newline characters when reading from file
+    *
+    * @return the payload of the json object to be created.
+    * @throws Exception
+    *            Any exception found will be thrown.
+    */
+   public static <T> T deserialiseJsonObjectFromFile(String filename, Class<T> clazz, boolean preserveWhitespace)
+         throws Exception {
+      return JsonUtil.deserialize(readFileAsString(filename, preserveWhitespace), clazz);
+   }
+
+   /**
+    * Method to read a Json file and return a String representation of the file.
+    * 
+    * @param filename
+    *           Name of file to be read
+    * @param preserveWhitespace
+    *           whether to include spaces/newline characters when reading from file
+    * 
+    * @return String representation of json file
+    * @throws Exception
+    *            Any exceptions found trying to read file will be thrown
+    */
+   public static String readFileAsString(String filename, boolean preserveWhitespace) throws Exception {
+      StringBuilder sb = new StringBuilder();
+      try (BufferedReader br = new BufferedReader(new FileReader(TEST_MSG_FILE_PATH + filename))) {
+         String line = br.readLine();
+         while (line != null) {
+            if (preserveWhitespace) {
+               sb.append(line);
+               sb.append(System.lineSeparator());
+            } else {
+               sb.append(line.trim());
+            }
+            line = br.readLine();
+         }
+      }
+      String fileContents = sb.toString();
+      if (preserveWhitespace) {
+         fileContents = fileContents.substring(0, fileContents.length() - 1);
+      }
+      return fileContents;
    }
 }
