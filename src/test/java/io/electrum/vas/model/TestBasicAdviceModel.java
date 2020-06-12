7903b93e68e22ec4275b132fe8730d1bd7ed09d8
Clayton <sibandaclayton0@gmail.com>
2020-06-12 14:02:10 +0200

    added test and testing constants

---
+++ src/test/java/io/electrum/vas/model/TestBasicAdviceModel.java
@@ -0,0 +1,58 @@
+package io.electrum.vas.model;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestBasicAdviceModel {
+    private BasicAdvice basicAdvice = null;
+
+    @Test
+    public void setup() throws IOException, IllegalAccessException {
+        basicAdvice = new BasicAdvice();
+        basicAdvice.setId(RandomStringUtils.random(16));
+        basicAdvice.setRequestId(RandomStringUtils.random(32));
+        basicAdvice.setRrn(RandomStringUtils.random(16));
+        basicAdvice.setStan(RandomStringUtils.random(16));
+        List<ThirdPartyIdentifier> thirdPartyIdentifiers = new ArrayList<ThirdPartyIdentifier>();
+        thirdPartyIdentifiers.add(new ThirdPartyIdentifier().institutionId(RandomStringUtils.random(16)).transactionIdentifier(RandomStringUtils.random(16)));
+        basicAdvice.setThirdPartyIdentifiers(thirdPartyIdentifiers);
+        basicAdvice.setAmounts(TestingConstants.AMOUNTS_FULL_REVERSAL);
+        NewModelTests modelTests = new NewModelTests();
+        modelTests.testDeserialiseSerialiseObject(TestingConstants.getSerializedFullReversalBasicAdvice(),BasicAdvice.class);
+        modelTests.testDeserialiseSerialiseObject(TestingConstants.getSerializedPartialReversalBasicAdvice(),BasicAdvice.class);
+        modelTests.testSerialiseDeserialiseObject(TestingConstants.getFullReversalAdvice());
+        modelTests.testSerialiseDeserialiseObject(TestingConstants.getPartialReversalAdvice());
+
+    }
+
+    public String serialize(Object object) throws IllegalAccessException {
+        if(object instanceof String || object.getClass().isPrimitive())
+        {
+            return "{"+object.getClass().getName();
+        }
+        StringBuilder serializedBasicAdvice = new StringBuilder();
+        serializedBasicAdvice.append("{");
+        Field[] declaredFields = object.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.get(object) != null) {
+                serializedBasicAdvice.append("\"").append(field.getName()).append("\":");
+                if(field.get(object) instanceof String)
+                    serializedBasicAdvice.append("\"").append(field.get(object)).append("\",");
+                else
+                    serializedBasicAdvice.append(field.get(object)).append(",");
+            }
+        }
+        serializedBasicAdvice.append("\"");
+        serializedBasicAdvice.append("}");
+
+        return serializedBasicAdvice.toString();
+    }
+
+
+
+}
---
+++ src/test/java/io/electrum/vas/model/TestingConstants.java
@@ -0,0 +1,71 @@
+package io.electrum.vas.model;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestingConstants {
+
+
+    //CONSTANTS FOR CONSTRUCTING A BASIC ADVICE INSTANCE
+
+    public static String ID = RandomStringUtils.randomAlphabetic(16);
+    public static String REQUEST_ID = RandomStringUtils.randomAlphabetic(16);
+    public static DateTime TIME = DateTime.now();
+    public static List<ThirdPartyIdentifier> THIRDPARTYIDENTIFIERS = Arrays.asList(getThirdPartyIdentifier(),getThirdPartyIdentifier());
+    public static String STAN = RandomStringUtils.randomAlphabetic(16);;
+    public static String RRN = RandomStringUtils.randomAlphabetic(16);;
+    public static Amounts AMOUNTS_FULL_REVERSAL = new Amounts();
+    public static Amounts AMOUNTS_PARTIAL_REVERSAL = new Amounts().approvedAmount(new LedgerAmount().amount(9000L).currency("710").ledgerIndicator(LedgerAmount.LedgerIndicator.DEBIT));
+
+    public static String getSerializedFullReversalBasicAdvice() {
+        StringBuilder SERIALIZED_BASIC_ADVICE = new StringBuilder().append("{")
+                .append("\"id\":").append("\"" + ID + "\",").append("\"requestId\":").append("\"" + REQUEST_ID + "\",").append("\"time\":")
+                .append("\"" + TIME.toString("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'") + "\",")
+                .append("\"thirdPartyIdentifiers\":").append("[{").append("\"institutionId\":").append("\"" + THIRDPARTYIDENTIFIERS.get(0).getInstitutionId() + "\",")
+                .append("\"transactionIdentifier\":").append("\"" + THIRDPARTYIDENTIFIERS.get(0).getTransactionIdentifier() + "\"").append("}],")
+                .append("\"stan\":").append("\"" + STAN + "\",").append("\"rrn\":").append("\"" + RRN + "\",")
+                .append("\"amounts\":").append("{").append("\"approvedAmount\":").append("{").append("\"amount\":").append(9000 + ",").append("\"currency\":").append("\"710\",")
+                .append("\"ledgerIndicator\":").append("\"DEBIT\"").append("}}}");
+
+        return SERIALIZED_BASIC_ADVICE.toString();
+    }
+
+    public static String getSerializedPartialReversalBasicAdvice() {
+        StringBuilder SERIALIZED_BASIC_ADVICE = new StringBuilder().append("{")
+                .append("\"id\":").append("\"" + ID + "\",").append("\"requestId\":").append("\"" + REQUEST_ID + "\",").append("\"time\":")
+                .append("\"" + TIME.toString("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'")+ "\",")
+                .append("\"thirdPartyIdentifiers\":").append("[{").append("\"institutionId\":").append("\"" + THIRDPARTYIDENTIFIERS.get(0).getInstitutionId() + "\",")
+                .append("\"transactionIdentifier\":").append("\"" + THIRDPARTYIDENTIFIERS.get(0).getTransactionIdentifier() + "\"").append("}],")
+                .append("\"stan\":").append("\"" + STAN + "\",").append("\"rrn\":").append("\"" + RRN + "\",")
+                .append("\"amounts\":").append("{").append("}}");
+
+        return SERIALIZED_BASIC_ADVICE.toString();
+    }
+
+    public static BasicAdvice getFullReversalAdvice(){
+        return new BasicAdvice()
+                .amounts(AMOUNTS_FULL_REVERSAL)
+                .id(ID).requestId(REQUEST_ID)
+                .time(TIME).rrn(RRN)
+                .stan(STAN)
+                .transactionIdentifiers(THIRDPARTYIDENTIFIERS);
+    }
+    public static BasicAdvice getPartialReversalAdvice(){
+        return new BasicAdvice()
+                .amounts(AMOUNTS_PARTIAL_REVERSAL)
+                .id(ID).requestId(REQUEST_ID)
+                .time(TIME).rrn(RRN)
+                .stan(STAN)
+                .transactionIdentifiers(THIRDPARTYIDENTIFIERS);
+    }
+
+    private static ThirdPartyIdentifier getThirdPartyIdentifier(){
+        return new ThirdPartyIdentifier()
+                .institutionId(RandomStringUtils.randomAlphanumeric(16))
+                .transactionIdentifier(RandomStringUtils.randomAlphanumeric(16));
+    }
+}
