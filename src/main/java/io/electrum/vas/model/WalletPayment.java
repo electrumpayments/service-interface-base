2cec3edd9358e1dc75ded03f5305f990811b9007
TimElectrum <timwhelan@electrum.co.za>
2021-06-14 12:08:59 +0200

    fixed code indentation in `WalletPayment`

--- src/main/java/io/electrum/vas/model/WalletPayment.java
+++ src/main/java/io/electrum/vas/model/WalletPayment.java
@@ -14,53 +14,55 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description = "Model for mobile wallet payments", parent = PaymentMethod.class)
 public class WalletPayment extends PaymentMethod {
 
-    private String walletId;
+   private String walletId;
 
-    public WalletPayment(){
-        setType(PaymentMethodType.WALLET);
-    }
+   public WalletPayment() {
+      setType(PaymentMethodType.WALLET);
+   }
 
-    public WalletPayment walletId(String walletId) {
-        this.walletId = walletId;
-        return this;
-    }
+   public WalletPayment walletId(String walletId) {
+      this.walletId = walletId;
+      return this;
+   }
 
+   @ApiModelProperty(required = true, value = "The unique identifier of the wallet account making the payment.")
+   @JsonProperty("walletId")
+   @NotNull
+   @Masked
+   public String getWalletId() {
+      return walletId;
+   }
 
-    @ApiModelProperty(required = true, value = "The unique identifier of the wallet account making the payment.")
-    @JsonProperty("walletId")
-    @NotNull
-    @Masked
-    public String getWalletId() {
-        return walletId;
-    }
+   @Override
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (o == null || getClass() != o.getClass())
+         return false;
+      if (!super.equals(o))
+         return false;
+      WalletPayment that = (WalletPayment) o;
+      return Objects.equals(walletId, that.walletId);
+   }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        WalletPayment that = (WalletPayment) o;
-        return Objects.equals(walletId, that.walletId);
-    }
+   @Override
+   public int hashCode() {
+      return Objects.hash(super.hashCode(), walletId);
+   }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), walletId);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class WalletPayment {\n");
-        sb.append("    name: ").append(Utils.toIndentedString(name)).append('\n');
-        sb.append("    type: ").append(Utils.toIndentedString(type)).append('\n');
-        sb.append("    amount: ").append(Utils.toIndentedString(amount)).append('\n');
-        sb.append("    issuer: ").append(Utils.toIndentedString(issuer)).append('\n');
-        sb.append("    pin: ").append(Utils.toIndentedString(pin)).append('\n');
-        sb.append("    proxy: ").append(Utils.toIndentedString(proxy)).append('\n');
-        sb.append("    proxyType: ").append(Utils.toIndentedString(proxyType)).append('\n');
-        sb.append("    walletId: ").append(Utils.toIndentedString(walletId)).append('\n');
-        sb.append('}');
-        return sb.toString();
-    }
+   @Override
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("class WalletPayment {\n");
+      sb.append("    name: ").append(Utils.toIndentedString(name)).append('\n');
+      sb.append("    type: ").append(Utils.toIndentedString(type)).append('\n');
+      sb.append("    amount: ").append(Utils.toIndentedString(amount)).append('\n');
+      sb.append("    issuer: ").append(Utils.toIndentedString(issuer)).append('\n');
+      sb.append("    pin: ").append(Utils.toIndentedString(pin)).append('\n');
+      sb.append("    proxy: ").append(Utils.toIndentedString(proxy)).append('\n');
+      sb.append("    proxyType: ").append(Utils.toIndentedString(proxyType)).append('\n');
+      sb.append("    walletId: ").append(Utils.toIndentedString(walletId)).append('\n');
+      sb.append('}');
+      return sb.toString();
+   }
 }
